fc8f6b07bb930492eddc5ef1f85c1aac59456230
Jac Goudsmit <jac_goudsmit@yahoo.com>
2014-01-18 16:14:23 -0800

    Improvements to the sketch

--- HeeetSeeeker/HeeetSeeeker.ino
+++ HeeetSeeeker/HeeetSeeeker.ino
@@ -13,6 +13,9 @@ unsigned short sentences, failed_checksum;
 const double HEATSYNC_LAT = 33.41532;
 const double HEATSYNC_LON = -111.835625;
 
+const uint32_t MAX_AGE = 30000;
+const uint32_t UPDATE_INTERVAL = 1000;
+
 extern unsigned char hsllogo[] PROGMEM;
 
 void setup(){
@@ -24,45 +27,70 @@ void setup(){
   PORTB |= 0x21;
 
   SeeedOled.clearDisplay();
-  SeeedOled.drawBitmap(hsllogo, 1024);
-
   SeeedOled.setNormalDisplay();  
   SeeedOled.setPageMode();
-  SeeedOled.setTextXY(3,3);
-  SeeedOled.putString("HeatSeeker");
-  
-  delay(1000);
-  
-  SeeedOled.clearDisplay();
+
+  splashscreen("HeeetSeeeker");
 }
 
-void loop() {
-  if (Serial1.available()){
-    if(gps.encode(Serial1.read())){
-      //every second, update the distance
-      delay(1000);
-      
-      
-      //calculate distance in meters*
-      double dist = 
-          gps.distanceBetween(
-              gps.location.lat(),
-              gps.location.lng(),
-              HEATSYNC_LAT,
-              HEATSYNC_LON) / 1000000.0;
-          
-       
-       //write the text, inform the user
-       SeeedOled.setTextXY(0,3);
-       SeeedOled.putString("Distance in m");
-       SeeedOled.setTextXY(1,3);
-       SeeedOled.putString("to HeatSync:");
-       
-       SeeedOled.setTextXY(2,3);
-       SeeedOled.putNumber(dist);
+void loop() 
+{
+  while (Serial.available())
+  {
+    gps.encode(Serial1.read());
+  }
 
-     
+  uint32_t age = gps.location.age();
+
+  if (age > MAX_AGE)
+  {
+    static uint32_t ms = 0;
+
+    if (((!ms) && (millis() > 6000))
+      || (millis() - ms > 2000))
+    {
+      ms = millis();
+
+      static bool b = false;
+      if (!(b = !b))
+      {
+        splashscreen("no GPS...");
+      }
+      else
+      {
+        splashscreen("seeeking :)");
+      }
     }
- }   
+  }
+  else if (age < UPDATE_INTERVAL)
+  {
+    //calculate distance in meters*
+    double dist = 
+        gps.distanceBetween(
+            gps.location.lat(),
+            gps.location.lng(),
+            HEATSYNC_LAT,
+            HEATSYNC_LON) / 1000000.0;
+       
+    //write the text, inform the user
+    SeeedOled.setTextXY(0,3);
+    SeeedOled.putString("Distance in m");
+    SeeedOled.setTextXY(1,3);
+    SeeedOled.putString("to HeatSync:");
+       
+    SeeedOled.setTextXY(2,3);
+    SeeedOled.putNumber(dist);
+  }
+  else
+  {
+    // Not enough time elapsed for update; don't do anything
+  }
 }
 
+void splashscreen(const char *s)
+{
+  SeeedOled.setTextXY(0,0);
+  SeeedOled.drawBitmap(hsllogo, 1024);
+  SeeedOled.setTextXY(3,8 - (strlen(s) >> 1));
+  SeeedOled.putString(s);
+}
